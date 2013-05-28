d1d401018205603947db51bac70931822de7f67c
rongguo <rongguo@rongguo-HP-Compaq-8200-Elite-SFF-PC.(none)>
2013-05-28 17:53:27 +0800

    Apue chapter 5 exercises change in fileIO
    As the title.

--- apue/fileIO/filemask.c
+++ apue/fileIO/filemask.c
@@ -1,6 +1,6 @@
 #include "../apue.h"
 #include <fcntl.h>
-
+#include <sys/stat.h>
 void changemode()
 {
      struct stat statbuf;
@@ -16,41 +16,91 @@ void changemode()
 	  printf("chmod foo2 error !\n");
 	  goto back2;
      }
-
+     getchar();
 
      if (chmod("bar", RWRWRW) < 0)
      {
 	  printf("chmod error for bar!\n");
 	  goto back2;
      }
+
 back2:
      return;
 }
 
+void checkFile(struct stat buf)
+{
+     char* ptr;
+     if (S_ISREG(buf.st_mode)) {
+	  ptr = "regular";
+     } else if (S_ISDIR(buf.st_mode)) {
+	  ptr = "directory";
+     } else if (S_ISCHR(buf.st_mode)) {
+	  ptr = "character special";
+     } else if (S_ISBLK(buf.st_mode)) {
+	  ptr = "block special";
+     } else if (S_ISFIFO(buf.st_mode)) {
+	  ptr = "fifo";
+     } else if (S_ISLNK(buf.st_mode)) {
+	  ptr = "symbolic link";
+     } else if (S_ISSOCK(buf.st_mode)) {
+	  ptr = "socket";
+     } else {
+	  ptr = "** unknow mode **";
+     }
+     printf("\t%s\n", ptr);
+}
+
+void umask777()
+{
+     struct stat buf;
+     int mode;
+//     umask(0777);
+     if (creat("foo3", RWXRWXRWX) < 0)
+     {
+	  printf("creat error for foo3\n");
+     }
+     getchar();
+     if (stat("foo3", &buf) < 0)
+     {
+	  printf("stat error for foo3\n");
+     }
+     checkFile(buf);
+     getchar();
+     if (chmod("foo3", RWRWRW) < 0)
+     {
+	  printf("chmod error for foo3\n");
+     }
+     mode = umask(0);
+     printf("umask(0) = %o\n", mode);
+}
+
 int main(int argc, char **argv)
 {
+     char t;
      mode_t mode;
-     mode = umask(0);
+     mode = umask(0777);
      printf("umask(0) = %o\n", mode);
      if (creat("foo2",RWXRWXRWX) < 0)
      {
 	  printf("create error for foo2\n");
 	  goto back;
      }
-
-
-     mode = umask(S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH);
+     getchar();
+//     mode = umask(S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH);
      printf("umask(0) = %o\n", mode);
      if (creat("bar", RWXRWXRWX) < 0)
      {
 	  printf("create error for bar\n");
 	  goto back;
      }
-     mode = umask(044);
+     getchar();
+//     mode = umask(044);
      printf("umask(0) = %o\n", mode);
-     mode = umask(055);
+//     mode = umask(055);
      printf("umask(0) = %o\n", mode);
      changemode();
+     umask777();
 back:
      exit(0);
 }
--- apue/fileIO/filetype.c
+++ apue/fileIO/filetype.c
@@ -1,37 +1,90 @@
 #include "../apue.h"
 #include <sys/stat.h>
+#include <errno.h>
+
+void checkFile(struct stat buf)
+{
+     char* ptr;
+     if (S_ISREG(buf.st_mode)) {
+	  ptr = "regular";
+     } else if (S_ISDIR(buf.st_mode)) {
+	  ptr = "directory";
+	  printf("direcotry file length: %d\n", buf.st_size);
+     } else if (S_ISCHR(buf.st_mode)) {
+	  ptr = "character special";
+     } else if (S_ISBLK(buf.st_mode)) {
+	  ptr = "block special";
+     } else if (S_ISFIFO(buf.st_mode)) {
+	  ptr = "fifo";
+     } else if (S_ISLNK(buf.st_mode)) {
+	  ptr = "symbolic link";
+	  printf("smybolic file length: %d\n", buf.st_size);
+     } else if (S_ISSOCK(buf.st_mode)) {
+	  ptr = "socket";
+     } else {
+	  ptr = "** unknow mode **";
+     }
+     printf("\t%s\n", ptr);
+}
+void lstatCheck(char argv[])
+{
+     struct stat buf;
+
+     printf("   lstatCheck\n");
+     if (lstat(argv, &buf) < 0) {
+	  printf("lstat error\n");
+	  return;
+     }
+     checkFile(buf);
+}
+
+void statCheck(char argv[])
+{
+     struct stat buf;
+     printf("    statCheck\n");
+
+     if (stat(argv, &buf) < 0)
+     {
+	  printf("stat error!\n");
+	  return;
+     }
+     checkFile(buf);
+}
 
 int main(int argc, char *argv[])
 {
      int i;
      struct stat buf;
      char * ptr;
+     int errno;
 
      for(i = 1; i < argc; i++) {
-	  printf("%s: ", argv[i]);
-	  if (lstat(argv[i], &buf) < 0) {
-	       printf("lstat error\n");
+	  printf("%s: \n", argv[i]);
+	  lstatCheck(argv[i]);
+	  statCheck(argv[i]);
+	  if (lstat(argv[i], &buf) < 0)
+	  {
+	       printf("stat error!\n");
 	       continue;
 	  }
-
-	  if (S_ISREG(buf.st_mode)) {
-	       ptr = "regular";
-	  } else if (S_ISDIR(buf.st_mode)) {
-	       ptr = "directory";
-	  } else if (S_ISCHR(buf.st_mode)) {
-	       ptr = "character special";
-	  } else if (S_ISBLK(buf.st_mode)) {
-	       ptr = "block special";
-	  } else if (S_ISFIFO(buf.st_mode)) {
-	       ptr = "fifo";
+	  if (S_ISDIR(buf.st_mode)) {
+	       if((errno = truncate(argv[i], 0)) < 0) {
+		    printf("truncate direcotry error = %s\n", strerror(errno));
+	       }
 	  } else if (S_ISLNK(buf.st_mode)) {
-	       ptr = "symbolic link";
-	  } else if (S_ISSOCK(buf.st_mode)) {
-	       ptr = "socket";
-	  } else {
-	       ptr = "** unknow mode **";
+	       printf("began truncate link file\n");
+	       if ((errno = truncate(argv[i], 0)) < 0) {
+		    printf("truncate link error = %s\n", strerror(errno));
+	       }
 	  }
-	  printf("%s\n", ptr);
+	  lstat(argv[i], &buf);
+	  printf("file size after truncate: %d\n", buf.st_size);
+
+     }
+
+     for (i = -1; i < 10 ; i++)
+     {
+	  printf("%d: %s\n", i, strerror(i));
      }
      exit(0);
 }
